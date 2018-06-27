ac020178cc6c7162aa489780f65dde1a837a12ba
paul <lalance.paul@gmail.com>
2018-06-27 21:53:23 +0200

    Ajout app.js + package.json

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
+});
+
+app.get('/sous-sol', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+
+app.get('/etage/1/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hé ho, c\'est privé ici !');
+});
+
+// Exemple route dynamique
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+});
+
+// 404 par défaut
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8080);
